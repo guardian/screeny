4724a12e34c0e7ca6cc5483f4460f6568cf893ab
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2019-07-24 16:20:53 +0100

    Refactor to use pupeteer cluster

--- index.js
+++ index.js
@@ -1,67 +1,56 @@
 const takeScreenshot = require('take-screenshots');
 const mergeImg = require('merge-img');
 const fetch = require("node-fetch");
+const { Cluster } = require('puppeteer-cluster');
+
 
 // Config
 const ophanAPI = 'https://api.ophan.co.uk/api/mostread/keywordtag/tone%2Fnews?count=50';
-// const formatOphanUrl = url => url;
-const formatOphanUrl = url => url.replace('https://www.theguardian.com/', `http://localhost:3030/AmpArticle?url=https://www.theguardian.com/`);
-// const addSuffix = '?guui';
-const addSuffix = '';
+const formatOphanUrl = url => url;
+// const formatOphanUrl = url => url.replace('https://www.theguardian.com/', `http://localhost:3030/AmpArticle?url=https://www.theguardian.com/`);
+const addSuffix = '?guui';
+// const addSuffix = '';
+const clusterAmount = 10;
+const pupeteerScreenshotSettings = { fullPage: true };
 
 // Tracking
 let numOfUrl = 0;
 let numCompleted = 0;
 
-// Helper
-function delay(t, v) {
-    return new Promise(function(resolve) { 
-        setTimeout(resolve.bind(null, v), t)
-    });
- }
-
-
  // Go
 (async () => {
 
-    const urls = await fetch(ophanAPI).then(resp => resp.json())
+        const urls = await fetch(ophanAPI).then(resp => resp.json())
 
-    numOfUrl = urls.length;
+        numOfUrl = urls.length;
+        console.log(`${numOfUrl} URLs`)
 
-    let queue = Promise.resolve(); 
-
-    urls.forEach(function (urlObj) {
-        queue = queue.then(function (res) {
-            const url = formatOphanUrl(urlObj.url);
-            console.log("Taking a screenshot of ", url);
-            return takeScreens(url)
-        });
-    });
-
-    queue.then(function(){
-        console.log("Done!");
-    });
-    
-})();
-
-const takeScreens = async (url) => {
-    try {
+        const cluster = await Cluster.launch({
+            concurrency: Cluster.CONCURRENCY_CONTEXT,
+            maxConcurrency: clusterAmount
+          });
+        
+    await cluster.task(async ({ page, data }) => {
+      
+        
         let [img, img2] = await Promise.all([
-            takeScreenshot(url, { viewport: { width: 1400, height: 800 }, screenshot: { fullPage: true } }),
-            takeScreenshot(url + addSuffix, { viewport: { width: 1400, height: 800 }, screenshot: { fullPage: true } })
+             (async () => {console.log("Taking a screenshot of ", data.url); await page.goto(data.url); return await page.screenshot(pupeteerScreenshotSettings) })(),
+             (async () => { await page.goto(data.url2); return await page.screenshot(pupeteerScreenshotSettings) })(),
         ]);
-        await takeScreenshot.closeBrowser();
+              
         await mergeImg([img, img2])
             .then((img) => {
                 // Save image as file
-                img.write(`screenshots/${Math.random()}.png`, () => console.log(`${url} done`));
+                img.write(`screenshots/${Math.random()}.png`, () => console.log(`${data.url} done`));
                 console.log(`Completed ${numCompleted = numCompleted + 1} of ${numOfUrl}`)
-            });
-     
+        });
+    });
         
-        return true;
-    } catch {
-        return true;
-    }
-    
-    };
+    urls.forEach(function (urlObj) {
+        const url = formatOphanUrl(urlObj.url);
+        cluster.queue({ url, url2: `${url}${addSuffix}` });
+    });
+        
+    await cluster.idle();
+    await cluster.close();
+})();
