abb34b3449cd3c95ff5720d4138849218adf25f8
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2019-07-24 14:28:39 +0100

    Add slight refactor

--- index.js
+++ index.js
@@ -2,26 +2,39 @@ const takeScreenshot = require('take-screenshots');
 const mergeImg = require('merge-img');
 const fetch = require("node-fetch");
 
+// Config
 const ophanAPI = 'https://api.ophan.co.uk/api/mostread/keywordtag/tone%2Fnews?count=50';
+// const formatOphanUrl = url => url;
+const formatOphanUrl = url => url.replace('https://www.theguardian.com/', `http://localhost:3030/AmpArticle?url=https://www.theguardian.com/`);
+// const addSuffix = '?guui';
+const addSuffix = '';
 
+// Tracking
+let numOfUrl = 0;
+let numCompleted = 0;
+
+// Helper
 function delay(t, v) {
     return new Promise(function(resolve) { 
         setTimeout(resolve.bind(null, v), t)
     });
  }
 
+
+ // Go
 (async () => {
 
     const urls = await fetch(ophanAPI).then(resp => resp.json())
 
-    
+    numOfUrl = urls.length;
 
-    let queue = Promise.resolve(); // in ES6 or BB, just Promise.resolve();
+    let queue = Promise.resolve(); 
 
-    urls.forEach(function(urlObj){
-        queue = queue.then(function(res){
-            console.log("Calling async func for", urlObj.url);
-            return takeScreens(urlObj.url)
+    urls.forEach(function (urlObj) {
+        queue = queue.then(function (res) {
+            const url = formatOphanUrl(urlObj.url);
+            console.log("Taking a screenshot of ", url);
+            return takeScreens(url)
         });
     });
 
@@ -32,17 +45,23 @@ function delay(t, v) {
 })();
 
 const takeScreens = async (url) => {
-    let img = await takeScreenshot(url, { viewport: { width: 1400, height: 800 }, screenshot: { fullPage: true } });
-    await delay(1000);
-    let img2 = await takeScreenshot(`${url}?guui`, {viewport: {width: 1400, height: 800}, screenshot: { fullPage: true } });
-    await delay(1000);
-    await mergeImg([img, img2])
-        .then((img) => {
-            console.log(img)
-            // Save image as file
-            img.write(`screenshots/${Math.random()}.png`, () => console.log(`${url} done`));
-        });
- 
-    await takeScreenshot.closeBrowser();
-    return true;
+    try {
+        let [img, img2] = await Promise.all([
+            takeScreenshot(url, { viewport: { width: 1400, height: 800 }, screenshot: { fullPage: true } }),
+            takeScreenshot(url + addSuffix, { viewport: { width: 1400, height: 800 }, screenshot: { fullPage: true } })
+        ]);
+        await takeScreenshot.closeBrowser();
+        await mergeImg([img, img2])
+            .then((img) => {
+                // Save image as file
+                img.write(`screenshots/${Math.random()}.png`, () => console.log(`${url} done`));
+                console.log(`Completed ${numCompleted = numCompleted + 1} of ${numOfUrl}`)
+            });
+     
+        
+        return true;
+    } catch {
+        return true;
+    }
+    
     };
